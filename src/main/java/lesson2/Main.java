ef299501057dce44d8b102105155636382eadecc
Nurulla Osmanov <Shivaii@yandex.ru>
2022-11-22 21:45:48 +0300

    lesson 2 task 5

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -8,6 +8,7 @@ public class Main {
         arrTask2();
         arrTask3();
         arrTask4();
+        arrTask5();
     }
 
     /*
@@ -17,10 +18,8 @@ public class Main {
     public static void arrTask1() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1)
-                arr[i] = 0;
-            else
-                arr[i] = 1;
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
         }
         System.out.println(Arrays.toString(arr));
     }
@@ -42,12 +41,12 @@ public class Main {
     public static void arrTask3() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6)
-                arr[i] *= 2;
+            if (arr[i] < 6) arr[i] *= 2;
         }
         System.out.println(Arrays.toString(arr));
     }
-    /*4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+    /*
+    4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
     и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
      */
 
@@ -55,13 +54,31 @@ public class Main {
         int[][] arr = new int[4][4];
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == arr[j])
-                    arr[i][j] = 1;
+                if (arr[i] == arr[j]) arr[i][j] = 1;
             }
         }
         for (int[] i : arr) {
             System.out.println(Arrays.toString(i));
         }
     }
-//лорпо
+    /*
+    5. Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+    */
+
+    public static void arrTask5() {
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        /*int max = 0;
+        int min = arr[0];
+        for (int j : arr) {
+            if (j > max) {
+                max = j;
+            } else {
+                min = j;
+            }
+        }*/
+        Arrays.sort(arr);
+        int min = arr[0];
+        int max = arr.length - 1;
+        System.out.println("Максимальный элемент = " + max + ". Mинимальный элемент = " + min);
+    }
 }
